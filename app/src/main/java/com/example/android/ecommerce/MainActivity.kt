286cfc58b959e2a1a143b48a9b661ef2cf8b7386
Ritik Rawat <75121026+ritikrawat2447@users.noreply.github.com>
2022-12-07 23:18:05 +0530

    initial commit

---
+++ app/src/main/java/com/example/android/ecommerce/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.android.ecommerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.android.ecommerce.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController  = navHostFragment!!.findNavController()
+
+        val popUpMenu = PopupMenu(this,null)
+        popUpMenu.inflate(R.menu.bottom_nav)
+        binding.bottomBar.setupWithNavController(popUpMenu.menu,navController)
+
+        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+                title = when(destination.id){
+                    R.id.cartFragment -> "My Cart"
+                    R.id.moreFragment -> "My Dashboard"
+                    else -> "no"
+                }
+            }
+
+        })
+
+    }
+}
